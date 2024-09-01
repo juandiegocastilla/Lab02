c7aa453608805a53194ec869126e8f66ebb77126
juandiegocastilla <est.juan.castilla@unimilitar.edu.co>
2024-09-01 18:36:14 -0500

    Creacion menu

---
+++ src/main/java/ej01/Calculo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej01;
+
+/**
+ *
+ * @author juand
+ */
+public class Calculo {
+    
+}
